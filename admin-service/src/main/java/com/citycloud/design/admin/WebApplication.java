a40ca992430cf2d8244fbd8371bfa0177cbd5c34
mengshuai <mengshuai@citycloud.com.cn>
2022-04-19 10:45:56 +0800

    api模块引用包名修改

--- admin-service/src/main/java/com/citycloud/design/admin/WebApplication.java
+++ admin-service/src/main/java/com/citycloud/design/admin/WebApplication.java
@@ -1,9 +1,14 @@
 package com.citycloud.design.admin;
 
+import com.citycloud.ccuap.web.api.annotation.ApiController;
+import com.citycloud.ccuap.web.api.annotation.QueryMapping;
+import com.citycloud.ccuap.web.api.response.ApiResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+import java.util.Random;
 
 @SpringBootApplication(scanBasePackages = {"com.citycloud"},
         exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
@@ -11,6 +16,15 @@ public class WebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
+    }
+
+
+    @ApiController(path = "/test")
+    public class Test {
+        @QueryMapping("/test")
+        public ApiResponse<String> getAa() {
+            return ApiResponse.successWithData("hello " + new Random().nextInt(100));
+        }
     }
 
 }
